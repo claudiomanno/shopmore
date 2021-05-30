3bc3101133e9ceac22c9d3056167a58282d3fdd8
cld <cmanno@xeniaprogetti.it>
2021-05-30 13:13:14 +0200

    fix-comment

--- svelte-login/src/pages/_layout.svelte
+++ svelte-login/src/pages/_layout.svelte
@@ -1,20 +1,7 @@
-<<<<<<< Updated upstream
-
 <script>
     import Nav from '../components/NavBar.svelte';
     import Footer from '../components/Footer.svelte';
 </script>
-
 <Nav />
 <slot></slot>
-=======
-
-<script>
-    import Nav from '../components/NavBar.svelte';
-    import Footer from '../components/Footer.svelte';
-</script>
- 
-<Nav />
-<slot></slot>
->>>>>>> Stashed changes
 <Footer />
